a24b1ad5b71942ccf33261ba7a7a5ddebdcaf177
sevos83 <sev83alex@gmail.com>
2021-07-22 15:45:42 +0400

    guru7

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
-  validates :title, presence: true, uniqueness: {scope: level}
+  validates :title, presence: true, uniqueness: {scope: :level}
   validates :level, numericality: {only_integer: true, greater_than: 0}
 
   has_many :questions
@@ -7,13 +7,6 @@ class Test < ApplicationRecord
   has_many :users, through: :completed_tests
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-
-  def self.list_names(category)
-    joins(:category)
-        .where(categories: {title: category})
-        .order('tests.title DESC')
-        .pluck(:title)
-  end
 
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
