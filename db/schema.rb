fe02bfad2825544212d720344090900c2d973772
sevos83 <sev83alex@gmail.com>
2021-07-24 18:29:56 +0400

    test7.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_201107) do
+ActiveRecord::Schema.define(version: 2021_07_24_142301) do
 
   create_table "answers", force: :cascade do |t|
     t.string "answer", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_07_05_201107) do
     t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "user_tests", force: :cascade do |t|
