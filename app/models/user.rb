9a76d8ba18ef192f54be8203836fafdb13d8df9d
sevos83 <sev83alex@gmail.com>
2021-07-24 20:24:13 +0400

    test7.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  has_many :user_tests
-  has_many :tests, through: :user_tests
+  validates :name, :email, presence: true
+
+  has_many :completed_tests
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
-  def tests_by_level(level)
+  has_many :tests, through: :completed_tests
+
+  def test_by_level(level)
     tests.where(level: level)
   end
 end
