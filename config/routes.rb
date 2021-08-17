e3759680522cb4ccef99498bcaf5a40a41daeef3
sevos83 <sev83alex@gmail.com>
2021-08-17 13:43:49 +0400

    test9.1

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, expect: :index
-  end
+      resources :answers, shallow: true, except: :index
+    end
 
-  member do
-    post :start
+    member do
+      post :start
+    end
   end
 
   resources :test_passages, only: %i[show update] do
